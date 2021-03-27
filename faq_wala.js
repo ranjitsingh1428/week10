fba546e878cf7dd14dd127a9438fc1726af97977
Ranjit singh <Ranjit>
2021-03-27 18:44:10 -0400

    third

---
+++ faq_wala.js
@@ -0,0 +1,30 @@
+var $ = function (id) { return document.getElementById(id); };
+
+var toggle = function () {
+    var h2 = this;                    // clicked h2 tag
+    var div = h2.nextElementSibling;  // h2's sibling div
+
+    if (h2.hasAttribute("class")) { 
+        h2.removeAttribute("class");	
+    } else { 
+        h2.setAttribute("class", "minus"); 
+    }
+
+    if (div.hasAttribute("class")) { 
+        div.removeAttribute("class");
+    } else { 
+        div.setAttribute("class", "open"); 
+    } 
+};
+window.onload = function () {
+    // get the h2 tags
+    var faqs = $("faqs");
+    var h2Elements = faqs.getElementsByTagName("h2");
+    
+    // attach event handler for each h2 tag	    
+    for (var i = 0; i < h2Elements.length; i++ ) {
+        h2Elements[i].onclick = toggle;
+    }
+    // set focus on first h2 tag's <a> tag
+    h2Elements[0].firstChild.focus();       
+};
